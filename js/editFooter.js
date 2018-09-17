490ddba3902403047e473c8b574eafc8744b77f2
Yaroslav Zinchenko <zin.yar@mail.ru>
2018-09-17 20:59:48 +0300

    Copy from yaroslavzinchenko.github.io to yaroslavzinchenko.github.io/translations

---
+++ js/editFooter.js
@@ -0,0 +1,5 @@
+var currentDate = document.createTextNode("2018");
+
+var PFooterElement = document.getElementById('PFooterElement');
+
+PFooterElement.insertBefore(currentDate, PFooterElement.firstChild);
---
+++ js/menuController.js
@@ -0,0 +1,22 @@
+var iconMenuEnabled = true;
+
+function changeIcon() {
+
+	if (iconMenuEnabled) {
+		document.getElementById('dropdown-button-id').style.background = "url('https://yaroslavzinchenko.github.io/icons/cancel.png') center no-repeat";
+		document.getElementById('dropdown-button-id').style.backgroundSize = '61%';
+		iconMenuEnabled = false;
+
+	} else {
+		document.getElementById('dropdown-button-id').style.background = "url('https://yaroslavzinchenko.github.io/icons/menu-lines.png') center no-repeat";
+		document.getElementById('dropdown-button-id').style.backgroundSize = '75%';
+		iconMenuEnabled = true;
+	}
+	
+}
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content. */
+function dropdownController() {
+	document.getElementById("myDropdown").classList.toggle("show");
+}
